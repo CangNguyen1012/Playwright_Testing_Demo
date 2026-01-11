0229201c1c9c35d5c3cb7e7494b168480a1f26d5
CangNguyen <brunomessi933@gmail.com>
2026-01-11 17:14:37 +0700

    update code v2 11/01/2026

--- tests/testAPI.spec.ts
+++ tests/testAPI.spec.ts
@@ -6,8 +6,7 @@ test.describe("API test - expect", () => {
       "https://movienew.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP01",
       {
         headers: {
-          TokenCybersoft:
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJUZXN0aW5nIDA5IiwiSGV0SGFuU3RyaW5nIjoiMTcvMDYvMjAyNiIsIkhldEhhblRpbWUiOiIxNzgxNjU0NDAwMDAwIiwibmJmIjoxNzU3NzgyODAwLCJleHAiOjE3ODE4MDIwMDB9.-_5VIe7kzZRPNtHEjW0NXKsmWqPh8yyd-pUQ9bQfMrM",
+          TokenCybersoft: process.env.TOKEN_CYBERSOFT as string,
         },
       }
     );
